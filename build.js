b2425384cff43a6f1883cfe7e26e78343eb04326
John Otander <johnotander@gmail.com>
2015-10-20 11:42:07 -0600

    Begin reworking the build and components gen

--- build.js
+++ build.js
@@ -12,20 +12,18 @@ var conditionals = require('postcss-conditionals')
 var cssvariables = require('postcss-css-variables')
 var customMedia = require('postcss-custom-media')
 
+var componentsBuild = require('./components-build')
+
 // css to be processed
 var css = fs.readFileSync('src/tachyons.css', 'utf8')
 
 // process css
-var output = postcss([autoprefixer])
-  .use(atImport())
-  .use(cssvariables())
-  .use(conditionals())
-  .use(customMedia())
-  .process(css, {
-    from: 'src/tachyons.css',
-    to: 'css/tachyons.css'
-  })
-  .css
+var output = postcss([
+  atImport(), cssvariables(), conditionals(), customMedia(), autoprefixer()
+]).process(css, {
+  from: 'src/tachyons.css',
+  to: 'css/tachyons.css'
+}).css
 
 // get the original css size
 fs.writeFile('css/tachyons.css', output, 'utf-8')
@@ -36,11 +34,10 @@ console.log('This file starts out at ' + filesize(uncompressedSize) + ' which wo
 
 // minify the css
 new compressor.minify({
-    type: 'sqwish',
-    fileIn: 'css/tachyons.css',
-    fileOut: 'css/tachyons.min.css',
-    callback: function (err, min) {
-    }
+  type: 'sqwish',
+  fileIn: 'css/tachyons.css',
+  fileOut: 'css/tachyons.min.css',
+  callback: function (err, min) {}
 })
 
 var minified = fs.statSync('css/tachyons.min.css', 'utf8')
@@ -49,3 +46,5 @@ var minifiedSize = minified['size']
 
 console.log('After minification it is ' + filesize(minifiedSize))
 console.log('After gzipping it is ' + filesize(gzipped)  + 'instead of ' + filesize(gzipped))
+
+componentsBuild()
