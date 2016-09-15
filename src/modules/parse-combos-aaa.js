e0e02a4f865b9b606ff3dc3e015286b120ede862
mrmrs <hi@mrmrs.cc>
2016-09-15 16:24:16 +0100

    Add new css

--- src/modules/parse-combos-aaa.js
+++ src/modules/parse-combos-aaa.js
@@ -5,7 +5,7 @@ module.exports = function(colors) {
 
   var combos = []
   // Get the Colorable array
-  var arr = colorable(colors, { threshold: 3.0 })
+  var arr = colorable(colors, { threshold: 7.0 })
 
   // Flatten the array and combine the foreground and background colors
   arr.forEach(function(color) {
